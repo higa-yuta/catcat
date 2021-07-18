c6f27bdc02213f08523640998b894d84146e97e6
yuta higa <h.yuta2314@gmail.com>
2021-07-18 11:24:46 +0900

    minor modifed

--- test/controllers/homes_controller_test.rb
+++ test/controllers/homes_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class HomesControllerTest < ActionDispatch::IntegrationTest
   test "should get main" do
-    get homes_main_url
+    get root_path
     assert_response :success
   end
 
