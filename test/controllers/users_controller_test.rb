20ea7af920c8303a00dbe3bb334d995f738debea
yuta higa <h.yuta2314@gmail.com>
2021-08-14 16:21:42 +0900

    Add should_patch_update test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,15 +4,16 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   
   def setup
     @user = users(:okinawa)
-    @other_user = { name: "沖縄　次郎",
-                    email: "jiro@okinawa.com" }
+    @other_user = users(:tokyo)
   end
 
+#new
   test "should get new" do
     get signup_path
     assert_template 'users/new'
   end
 
+#show
   test "should get show when logged in" do
     log_in_as(@user)
     get user_path(@user)
@@ -25,6 +26,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_path
   end
 
+#edit
   test "should get edit when logged in" do
     log_in_as(@user)
     get edit_user_path(@user)
@@ -37,6 +39,18 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_path
   end
 
+#update
+  test "should patch update" do
+    log_in_as(@user)
+    patch user_path(@user), params: { user: { name: "沖縄　次郎",
+                                              email: "okinawa-jiro@gmail.com"}}
+    assert_not flash.empty?
+    assert_redirected_to user_path(@user)
+    @user.reload
+    assert_equal "沖縄　次郎", @user.name
+    assert_equal "okinawa-jiro@gmail.com", @user.email
+  end
+
   test "should reidrect update when not logged in" do
     patch user_path(@user), params: { user: { name: '沖縄　花子',
                                               email: @user.email }}
@@ -44,6 +58,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_path
   end
 
+#destroy
   test "should delete destroy" do
     log_in_as(@user)
     assert_difference "User.count", -1 do
